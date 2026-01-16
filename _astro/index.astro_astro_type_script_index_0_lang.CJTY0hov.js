85c2ef04a7edff8f0be1bdafddb1db1223197016
Mohammed Alkebsi <moha.alkebsi@gmail.com>
2026-01-16 03:13:34 +0300

    First upload for my site. This version is not perfect, but is what I have for now and still a work in progress!

---
+++ _astro/index.astro_astro_type_script_index_0_lang.CJTY0hov.js
@@ -0,0 +1,1 @@
+import{S as r}from"./ScrollManager.BB1oFsBO.js";const o={intro:"#intro-spacer",work:"#work-section",about:"#about-section",contact:"#contact-section"},t=new r;t.sectionTrigger(0,o.intro);t.sectionTrigger(1,o.work);t.sectionTrigger(2,o.about);t.sectionTrigger(3,o.contact);
